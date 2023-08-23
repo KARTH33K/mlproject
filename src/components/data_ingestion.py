86aa256c1859a7127bab8652d4d8c2f08860ba1b
KARTH33K <kartheekmailbox2020@gmail.com>
2023-08-23 14:54:18 -0700

    Data_ingestion_Complete

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -18,18 +18,20 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info('Entered the data ingestion method')
+        logging.info("Entered the data ingestion method")
         try:
-            df = pd.read_csv('notebook/data/stud.csv')
+            df = pd.read_csv('notebook\data\stud.csv')
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index= False, header= True)
 
-            logging.info('Train Test Split initiated')
+            logging.info("Train Test Split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            test_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
 
             logging.info('Ingestion of data completed')
 
@@ -40,12 +42,12 @@ class DataIngestion:
 
 
         except Exception as e:
-            raise CustomException(e.sys)
+            raise CustomException(e, sys)
         
         
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion
+    obj.initiate_data_ingestion()
 
 
 
